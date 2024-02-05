36f24dc53aa3de126361ffc0f7997be1cbf8daad
uranias <uranias@NBS>
2024-02-05 16:16:22 +0300

    Add project files.

---
+++ Business/Concrete/CitizenManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+using MernisServiceReference;
+
+namespace Business.Concrete
+{
+    public class CitizenManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+            return client.TCKimlikNoDogrulaAsync(new TCKimlikNoDogrulaRequest(new TCKimlikNoDogrulaRequestBody(person.NationalIdentity,person.FirstName,person.LastName,person.DateOfBirthYear))).Result.Body.TCKimlikNoDogrulaResult;
+        }
+
+        public List<Person> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workspace/Program.cs
@@ -0,0 +1,75 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+
+
+        //string ogrenci1 = "Engin";
+        //string ogrenci2 = "Kerem";
+        //string ogrenci3 = "Berkay";
+
+     
+
+        //string[] ogrenciler = new string[3];
+        //ogrenciler[0] = "Engin";
+        //ogrenciler[1] = "Kerem";
+        //ogrenciler[2] = "Berkay";
+
+        //ogrenciler = new string[4];
+        //ogrenciler[3] = "İlker";
+
+        //for (int i = 0; i < ogrenciler.Length; i++)
+        //{
+        //    Console.WriteLine(ogrenciler[i]);
+        //}
+
+        //string[] sehirler1 = new[] { "Ankara", "İstanbul", "izmir" };
+        //string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+        //sehirler2 = sehirler1;
+        //sehirler1[0] = "Adana";
+        //Console.WriteLine(sehirler2[0]);
+
+        Person person1 = new Person();
+        person1.FirstName = "Kazım";
+        person1.LastName = "Başar";
+        person1.DateOfBirthYear = 1995;
+        person1.NationalIdentity = 123123;
+
+
+        Person person2 = new Person();
+        person2.FirstName = "Murat";
+
+        //foreach (string sehir in sehirler1)
+        //{
+        //    Console.WriteLine(sehir);
+        //}
+
+   
+        //List<string> yeniSehirler1 = new List<string> { "Ankara 1", "İstanbul 1", "İzmir 1" };
+        //yeniSehirler1.Add("Adana 1");
+        //foreach (var sehir in yeniSehirler1)
+        //{
+        //    Console.WriteLine(sehir);
+        //}
+
+        PttManager pttManager = new PttManager(new CitizenManager());
+        pttManager.GiveMask(person1);
+
+        Console.ReadLine();
+    }
+
+  
+
+//    static int Topla(int sayi1 = 5, int sayi2 = 10)
+//    {
+//        int sonuc = sayi1 + sayi2;
+//        Console.WriteLine("Toplam : " + sonuc.ToString());
+//        return sonuc;
+//    }
+
+}
